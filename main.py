f76e4f20504e9b43a112be38934c1b9cb4a603b1
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-07 15:13:05 +0900

    과목 출력

--- main.py
+++ main.py
@@ -1,25 +1,76 @@
-##파이썬 슬라이더
-jumin="011111-3001154"
-print("성별:"+jumin[7])
-print("연:"+jumin[0:2])
-print("생년월일"+jumin[:6])
-print("뒤 7자리"+jumin[7:])
-print("뒤 (뒤에서 부터 출력)7자리"+jumin[-7:])
+from Score import *
+#작업 선택 종류
+SEARCH, LIST, STATS, DATAMG, END = ('1', '2', '3', '4', 'x')
 
-##문자열 함수
-python="python is Amazing"
-print(python.upper())
-print(python.lower())
-print(python.isupper())
-print(len(python))
-print(python.replace("python","Java"))
-index=python.index("n")
-print(index)
-index=python.index("n",index+1)
-print(index)
+def find_student():
+    instid = input(">학번 입력: ")
+    val = get_student(instid)
+    sname, deptid = val #리스트 언팩킹
+    dname = get_dept(deptid)
+    print("학번: %s    성명: %s   소속학과: %s(%s)" %(instid, sname, dname, deptid))
 
-##find와 index의 차이
-# print(python.index('java'))오류를 출력
-print(python.find("java"))##-1로 주고 계속 진행
-##문자열 갯수 새기 
-print(python.count("n"))##총 몇개가 들어가는지 
+def selmenu01_find():
+    while True:
+        print("_"*50)
+        print("1: 학생검색    2: 학과검색    3: 교수검색    4: 과목검색    5: 성적검색    0: 복귀")
+        selno = input("<작업 선택> ")
+        if len(selno) == 0 or selno == '0':
+            return
+        if len(selno) >= 0 and type(int(selno)) is int:
+            selno = int(selno)
+            if selno == 1:
+                find_student()
+            elif selno == 2:
+                find_dept()
+            elif selno == 3:
+                find_prof()
+            elif selno == 4:
+                find_subject()
+            elif selno == 5:
+                find_stscore()
+            else:
+                print("!잘못된 입력!")
+
+
+def sel_task(): #작업 선택 메뉴
+    print("_"*50)
+    print("1:검색   2:현황   3:통계   4:데이터관리   x:종료")
+    selno = input("<작업 선택> ")
+    if len(selno) == 0:
+        return 'x'
+    else:
+        return selno
+def find_dept():
+    indeptid=input(">학과 번호 입력:")
+    dname=get_dept(indeptid)
+    print("학과명 :%s (%s)"%(dname,indeptid))
+
+def find_prof():
+    prof=input(">검색할 교수님을 입력:")
+    dname=get_prof(prof)
+    name,age=dname
+    print("교수님 성함 :%s (%s)"%(name,age))
+
+def find_subject():
+    insubject=input(">검색할 과목을 입력:")
+    subject=get_subject(insubject)
+    study,time,fulltime,FirstName=subject
+    print("[과목명] :%s %s %s %s"%(study,time,fulltime,FirstName))
+
+########## Main ################
+while True:         #작업 선택
+    selno = sel_task()      #작업 번호 선택
+    if selno == SEARCH:
+        selmenu01_find()
+    elif selno == LIST:
+        print(LIST)
+    elif selno == STATS:
+        print(STATS)
+    elif selno == DATAMG:
+        selmenu04_up()
+    elif selno == END:          #(x)끝내기
+        break
+    else:
+        break
+################################
+
