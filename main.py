dd27bb11cb30461907f48f68adc2dbf9db4cfc0f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-08 23:45:06 +0900

    06.08 파이썬 2-1에 타이틀까지함

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from Score import *
-#작업 선택 종류
+
+# 작업 선택 종류
 SEARCH, LIST, STATS, DATAMG, END = ('1', '2', '3', '4', 'x')
 
-def sel_task(): #작업 선택 메뉴
-    print("_"*50)
+
+def sel_task():  # 작업 선택 메뉴
+    print("_" * 50)
     print("1:검색   2:현황   3:통계   4:데이터관리   x:종료")
     selno = input("<작업 선택> ")
     if len(selno) == 0:
         return 'x'
     else:
         return selno
+
+
 def find_student():
     instid = input(">학번 입력: ")
     val = get_student(instid)
-    sname, deptid = val #리스트 언팩킹
+    sname, deptid = val  # 리스트 언팩킹
     dname = get_dept(deptid)
-    print("학번: %s    성명: %s   소속학과: %s(%s)" %(instid, sname, dname, deptid))
+    print("학번: %s    성명: %s   소속학과: %s(%s)" % (instid, sname, dname, deptid))
+
 
 def selmenu01_find():
     while True:
-        print("_"*50)
+        print("_" * 50)
         print("1: 학생검색    2: 학과검색    3: 교수검색    4: 과목검색    5: 성적검색    0: 복귀")
         selno = input("<작업 선택> ")
         if len(selno) == 0 or selno == '0':
@@ -38,9 +43,11 @@ def selmenu01_find():
                 find_stscore()
             else:
                 print("!잘못된 입력!")
+
+
 def List01_find():
     while True:
-        print("_"*50)
+        print("_" * 50)
         print("1:학생 성적 현황(학생 성적 통지서) 2.과목별 성적 현황 3.복귀")
         selno = input("<작업 선택> ")
         if len(selno) == 0 or selno == '0':
@@ -56,35 +63,42 @@ def List01_find():
             else:
                 print("!잘못된 입력!")
 
+
 ###########################################################2###################################################################
 def find_student():
     instid = input(">학번 입력: ")
     val = get_student(instid)
-    sname, deptid = val #리스트 언팩킹
+    sname, deptid = val  # 리스트 언팩킹
     dname = get_dept(deptid)
-    print("학번: %s    성명: %s   소속학과: %s(%s)" %(instid, sname, dname, deptid))
+    print("학번: %s    성명: %s   소속학과: %s(%s)" % (instid, sname, dname, deptid))
+
 
 def find_dept():
-    indeptid=input(">학과 번호 입력:")
-    dname=get_dept(indeptid)
-    print("학과명 :%s (%s)"%(dname,indeptid))
+    indeptid = input(">학과 번호 입력:")
+    dname = get_dept(indeptid)
+    print("학과명 :%s (%s)" % (dname, indeptid))
+
 
 def find_prof():
-    prof=input(">검색할 교수님을 입력:")
-    dname=get_prof(prof)
-    name,age=dname
-    print("교수님 성함 :%s (%s)"%(name,age))
+    prof = input(">검색할 교수님을 입력:")
+    dname = get_prof(prof)
+    name, age = dname
+    print("교수님 성함 :%s (%s)" % (name, age))
+
 
 def find_subject():
-    insubject=input(">검색할 과목을 입력:")
-    subject=get_subject(insubject)
-    study,time,fulltime,FirstName=subject
-    print("[과목명:%s] [학점 : %s] [이수 시간:%s] [ProF:%s]"%(study,time,fulltime,FirstName))
+    insubject = input(">검색할 과목을 입력:")
+    subject = get_subject(insubject)
+    study, time, fulltime, FirstName = subject
+    print("[과목명:%s] [학점 : %s] [이수 시간:%s] [ProF:%s]" % (study, time, fulltime, FirstName))
+
+
 def find_stscore():
-    instscore=input(">[학번]을 입력 해주세요")
-    for number,studynumber,score in stscore:
+    instscore = input(">[학번]을 입력 해주세요")
+    for number, studynumber, score in stscore:
         if instscore == number:
-            print("[번호 :%s] [과목 번호:%s] [점수:%s]"%(number,studynumber,score))
+            print("[번호 :%s] [과목 번호:%s] [점수:%s]" % (number, studynumber, score))
+
 
 ###########################################################2###################################################################
 def all_List():
@@ -92,12 +106,13 @@ def all_List():
     val = get_student(instid)
     sname, deptid = val
     dname = get_dept(deptid)
-    print("[학번: %s]    [성명: %s]   [소속학과: %s(%s)]" %(instid, sname, dname, deptid))
+    print("[학번: %s]    [성명: %s]   [소속학과: %s(%s)]" % (instid, sname, dname, deptid))
+    print("[과목명:%s] [과목코드:%s] [학점:%s] [점수:%s] [등급:%s]")
 
 
 ########## Main ################
-while True:         #작업 선택
-    selno = sel_task()      #작업 번호 선택
+while True:  # 작업 선택
+    selno = sel_task()  # 작업 번호 선택
     if selno == SEARCH:
         selmenu01_find()
     elif selno == LIST:
@@ -106,9 +121,8 @@ while True:         #작업 선택
         print(STATS)
     elif selno == DATAMG:
         selmenu04_up()
-    elif selno == END:          #(x)끝내기
+    elif selno == END:  # (x)끝내기
         break
     else:
         break
 ################################
-
