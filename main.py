baee1f4a6bed75b58469ebd247f377b0820b779a
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-09 21:54:27 +0900

    06.09 3-1시작해야됨

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ def selmenu01_find():
                 find_subject()
             elif selno == 5:
                 find_stscore()
+            elif selno == 0:
+                sel_task()
             else:
                 print("!잘못된 입력!")
 
@@ -47,7 +49,7 @@ def selmenu01_find():
 def List01_find():
     while True:
         print("_" * 50)
-        print("1:학생 성적 현황(학생 성적 통지서) 2.과목별 성적 현황 3.복귀")
+        print("1:학생 성적 현황(학생 성적 통지서) 2.과목별 성적 현황 0.복귀")
         selno = input("<작업 선택> ")
         if len(selno) == 0 or selno == '0':
             return
@@ -57,12 +59,28 @@ def List01_find():
                 all_List()
             elif selno == 2:
                 List_subject1()
-            elif selno == 3:
-                find_dept()
+            elif selno == 0:
+                sel_task()
             else:
                 print("!잘못된 입력!")
 
-
+def STATS01_find():
+    while True:
+        print("_" * 50)
+        print("1:과목별 성적 통계 2.교수별 담당 과목 0.복귀")
+        selno = input("<작업 선택> ")
+        if len(selno) == 0 or selno == '0':
+            return
+        if len(selno) >= 0 and type(int(selno)) is int:
+            selno = int(selno)
+            if selno == 1:
+                stats_find()
+            elif selno == 2:
+                List_subject1()
+            elif selno == 0:
+                sel_task()
+            else:
+                print("!잘못된 입력!")
 ###########################################################2###################################################################
 def find_student():
     instid = input(">학번 입력: ")
@@ -114,21 +132,21 @@ def all_List():
         subject = get_subject(studynumber)
         study, time, fulltime, FirstName = subject
         ascore=int(score)
-        if ascore <= 100 and ascore>= 95:
+        if ascore <= 100 and ascore >= 95:
             rank = "A+"
         elif ascore < 95 and ascore >= 90:
             rank = "A"
         elif ascore < 90 and ascore >= 85:
             rank = "B+"
-        elif ascore < 85 and ascore >=80:
+        elif ascore < 85 and ascore >= 80:
             rank = "B"
-        elif ascore < 80 and ascore >=75:
+        elif ascore < 80 and ascore >= 75:
             rank = "C+"
-        elif ascore < 75 and ascore >=70:
+        elif ascore < 75 and ascore >= 70:
             rank = "C"
-        elif ascore < 70 and ascore >=65:
+        elif ascore < 70 and ascore >= 65:
             rank = "D+"
-        elif ascore < 65 and ascore >=60:
+        elif ascore < 65 and ascore >= 60:
             rank = "D"
         else:
             rank = "F"
@@ -140,24 +158,32 @@ def all_List():
             sumscore=sumscore+int(score)##평균을 구하기 위해 형 변환
             Average=sumscore/count##평균 값
 
-            sumcredit = 0  ##평점 출력
             credit = 0
-            if rank == "A+":
-                credit += 4.5
-            elif rank == "A":
-                credit += 4.3
-            elif rank == "B+":
-                credit += 3.5
-            elif rank == "B":
-                credit += 3.3
-            elif rank == "C+":
-                credit += 2.5
-            elif rank == "C":
-                credit += 2.3
-            elif rank == "D+":
-                credit += 1.5
-            elif rank == "D":
-                credit += 1.3
+            sumcredit = 0
+            if Average >=95:
+                credit = 4.5
+                rank="A+"
+            elif Average >=90:
+                credit = 4.0
+                rank = "A"
+            elif Average >=85:
+                credit = 3.5
+                rank = "B+"
+            elif Average >=80:
+                credit = 3.0
+                rank="B"
+            elif Average>=75:
+                credit = 2.5
+                rank = "C+"
+            elif Average >=70:
+                credit = 2.0
+                rank = "C"
+            elif Average >=65:
+                credit = 1.5
+                rank = "D"
+            elif Average >=60:
+                credit = 1.0
+                rank = "D+"
             else:
                 credit = 0
     print("[과목 수:%d] [학점 수:%d] [평균 점수: %d] [평점%.1f]"%(count,counttime,Average,float(credit)))
@@ -166,11 +192,77 @@ def List_subject1():
     insubject = input(">검색할 과목코드를 입력해주세요:")
     subject = get_subject(insubject)
     study, time, fulltime, FirstName = subject
-    dname = get_prof(FirstName)
+    dname = get_prof(FirstName)##교수님 이름
     name, age = dname
-    dname = get_dept(fulltime)
-
+    dname = get_dept(fulltime)##학과 딕셔너리
     print("[과목명:%s] [과목코드: %s][학점 : %s] [개설 학과:%s] [교수님 이름:%s]" % (study,insubject,time,dname,name))
+    studycount=0
+    fullscore=0
+    for number, studynumber, score in stscore:
+        if studynumber==insubject:
+            studycount+=1
+            val = get_student(number)
+            sname, deptid = val  # 리스트 언팩킹
+            dname = get_dept(deptid)
+            ascore=float(score)
+            fullscore=fullscore+ascore
+            if ascore <= 100 and ascore >= 95:
+                rank = "A+"
+            elif ascore < 95 and ascore >= 90:
+                rank = "A"
+            elif ascore < 90 and ascore >= 85:
+                rank = "B+"
+            elif ascore < 85 and ascore >= 80:
+                rank = "B"
+            elif ascore < 80 and ascore >= 75:
+                rank = "C+"
+            elif ascore < 75 and ascore >= 70:
+                rank = "C"
+            elif ascore < 70 and ascore >= 65:
+                rank = "D+"
+            elif ascore < 65 and ascore >= 60:
+                rank = "D"
+            else:
+                rank = "F"
+            print("[학생성명:%s][학번:%s][점수:%s][등급:%s]"%(sname,number,score,rank))
+            Average = fullscore / studycount  ##평균 값
+            if Average >=95:
+                credit = 4.5
+                rank="A+"
+            elif Average >=90:
+                credit = 4.0
+                rank = "A"
+            elif Average >=85:
+                credit = 3.5
+                rank = "B+"
+            elif Average >=80:
+                credit = 3.0
+                rank="B"
+            elif Average>=75:
+                credit = 2.5
+                rank = "C+"
+            elif Average >=70:
+                credit = 2.0
+                rank = "C"
+            elif Average >=65:
+                credit = 1.5
+                rank = "D"
+            elif Average >=60:
+                credit = 1.0
+                rank = "D+"
+            else:
+                credit = 0
+    print("[학생 수:%s] [평균 점수:%.1f] [평점%.1f]"%(studycount,float(fullscore/studycount),credit))
+###########################################3##########################################################################
+
+def stats_find():
+    indeptid = input(">학과 번호 입력:")
+    dname = get_dept(indeptid)
+    print("[학과명 :%s (%s)]" % (dname, indeptid))
+    for number, studynumber, score in stscore:
+        subject = get_subject(studynumber)
+        study, time, fulltime, FirstName = subject
+        print("[과목명:%s] [학점 : %s] [이수 시간:%s] [ProF:%s]" % (study, time, fulltime, FirstName))
 
 
 ########## Main ################
@@ -181,7 +273,7 @@ while True:  # 작업 선택
     elif selno == LIST:
         List01_find()
     elif selno == STATS:
-        print(STATS)
+        STATS01_find()
     elif selno == DATAMG:
         selmenu04_up()
     elif selno == END:  # (x)끝내기
