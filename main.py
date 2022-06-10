e9e145888e4582054f475d0f2845c5863cd6c686
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-10 15:03:54 +0900

    06.09 3-2다 끝냄 근데 수강생 수를 구해야됨..

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Score import *
 # 작업 선택 종류
 SEARCH, LIST, STATS, DATAMG, END = ('1', '2', '3', '4', 'x')
 
+
 def sel_task():  # 작업 선택 메뉴
     print("_" * 50)
     print("1:검색   2:현황   3:통계   4:데이터관리   x:종료")
@@ -64,6 +65,7 @@ def List01_find():
             else:
                 print("!잘못된 입력!")
 
+
 def STATS01_find():
     while True:
         print("_" * 50)
@@ -76,11 +78,13 @@ def STATS01_find():
             if selno == 1:
                 stats_find()
             elif selno == 2:
-                List_subject1()
+                stats_find2()
             elif selno == 0:
                 sel_task()
             else:
                 print("!잘못된 입력!")
+
+
 ###########################################################2###################################################################
 def find_student():
     instid = input(">학번 입력: ")
@@ -124,14 +128,14 @@ def all_List():
     sname, deptid = val
     dname = get_dept(deptid)
     print("[학번: %s]    [성명: %s]   [소속학과: %s(%s)]" % (instid, sname, dname, deptid))
-    count=0
-    counttime=0
-    Average=0
-    sumscore=0
+    count = 0
+    counttime = 0
+    Average = 0
+    sumscore = 0
     for number, studynumber, score in stscore:
         subject = get_subject(studynumber)
         study, time, fulltime, FirstName = subject
-        ascore=int(score)
+        ascore = int(score)
         if ascore <= 100 and ascore >= 95:
             rank = "A+"
         elif ascore < 95 and ascore >= 90:
@@ -151,61 +155,62 @@ def all_List():
         else:
             rank = "F"
         if number == instid:
-            print("[과목명:%s] [과목코드 : %s] [학점:%s] [점수:%s] [등급:%s]" % (study, studynumber, time, score,rank))
-            count+=1
-            times=int(time)##학점을 더해주기 위해 형 변환
-            counttime=counttime+times##총 학점 수
-            sumscore=sumscore+int(score)##평균을 구하기 위해 형 변환
-            Average=sumscore/count##평균 값
+            print("[과목명:%s] [과목코드 : %s] [학점:%s] [점수:%s] [등급:%s]" % (study, studynumber, time, score, rank))
+            count += 1
+            times = int(time)  ##학점을 더해주기 위해 형 변환
+            counttime = counttime + times  ##총 학점 수
+            sumscore = sumscore + int(score)  ##평균을 구하기 위해 형 변환
+            Average = sumscore / count  ##평균 값
 
             credit = 0
             sumcredit = 0
-            if Average >=95:
+            if Average >= 95:
                 credit = 4.5
-                rank="A+"
-            elif Average >=90:
+                rank = "A+"
+            elif Average >= 90:
                 credit = 4.0
                 rank = "A"
-            elif Average >=85:
+            elif Average >= 85:
                 credit = 3.5
                 rank = "B+"
-            elif Average >=80:
+            elif Average >= 80:
                 credit = 3.0
-                rank="B"
-            elif Average>=75:
+                rank = "B"
+            elif Average >= 75:
                 credit = 2.5
                 rank = "C+"
-            elif Average >=70:
+            elif Average >= 70:
                 credit = 2.0
                 rank = "C"
-            elif Average >=65:
+            elif Average >= 65:
                 credit = 1.5
                 rank = "D"
-            elif Average >=60:
+            elif Average >= 60:
                 credit = 1.0
                 rank = "D+"
             else:
                 credit = 0
-    print("[과목 수:%d] [학점 수:%d] [평균 점수: %d] [평점%.1f]"%(count,counttime,Average,float(credit)))
+    print("[과목 수:%d] [학점 수:%d] [평균 점수: %d] [평점%.1f]" % (count, counttime, Average, float(credit)))
+
 
 def List_subject1():
     insubject = input(">검색할 과목코드를 입력해주세요:")
     subject = get_subject(insubject)
     study, time, fulltime, FirstName = subject
-    dname = get_prof(FirstName)##교수님 이름
+    dname = get_prof(FirstName)  ##교수님 이름
     name, age = dname
-    dname = get_dept(fulltime)##학과 딕셔너리
-    print("[과목명:%s] [과목코드: %s][학점 : %s] [개설 학과:%s] [교수님 이름:%s]" % (study,insubject,time,dname,name))
-    studycount=0
-    fullscore=0
+    dname = get_dept(fulltime)  ##학과 딕셔너리
+    print("[과목명:%s] [과목코드: %s][학점 : %s] [개설 학과:%s] [교수님 이름:%s]" % (study, insubject, time, dname, name))
+    studycount = 0
+    fullscore = 0
     for number, studynumber, score in stscore:
-        if studynumber==insubject:
-            studycount+=1
+        if studynumber == insubject:
+            studycount += 1
             val = get_student(number)
             sname, deptid = val  # 리스트 언팩킹
             dname = get_dept(deptid)
-            ascore=float(score)
-            fullscore=fullscore+ascore
+            ascore = float(score)
+            fullscore = fullscore + ascore
             if ascore <= 100 and ascore >= 95:
                 rank = "A+"
             elif ascore < 95 and ascore >= 90:
@@ -224,47 +229,73 @@ def List_subject1():
                 rank = "D"
             else:
                 rank = "F"
-            print("[학생성명:%s][학번:%s][점수:%s][등급:%s]"%(sname,number,score,rank))
+            print("[학생성명:%s][학번:%s][점수:%s][등급:%s]" % (sname, number, score, rank))
             Average = fullscore / studycount  ##평균 값
-            if Average >=95:
+            if Average >= 95:
                 credit = 4.5
-                rank="A+"
-            elif Average >=90:
+                rank = "A+"
+            elif Average >= 90:
                 credit = 4.0
                 rank = "A"
-            elif Average >=85:
+            elif Average >= 85:
                 credit = 3.5
                 rank = "B+"
-            elif Average >=80:
+            elif Average >= 80:
                 credit = 3.0
-                rank="B"
-            elif Average>=75:
+                rank = "B"
+            elif Average >= 75:
                 credit = 2.5
                 rank = "C+"
-            elif Average >=70:
+            elif Average >= 70:
                 credit = 2.0
                 rank = "C"
-            elif Average >=65:
+            elif Average >= 65:
                 credit = 1.5
                 rank = "D"
-            elif Average >=60:
+            elif Average >= 60:
                 credit = 1.0
                 rank = "D+"
             else:
                 credit = 0
-    print("[학생 수:%s] [평균 점수:%.1f] [평점%.1f]"%(studycount,float(fullscore/studycount),credit))
+    print("[학생 수:%s] [평균 점수:%.1f] [평점%.1f]" % (studycount, float(fullscore / studycount), credit))
+
+
 ###########################################3##########################################################################
 
 def stats_find():
+        indeptid = input(">학과 번호 입력:")
+        dname = get_dept(indeptid)
+        print("[학과명 :%s (%s)]" % (dname, indeptid))
+        for scode,val in subject.items():
+            studyname,time,studycode,Nikname=val
+            if indeptid in studycode:
+                arr = []
+                count = 0
+                Avarge = 0
+                allArv = 0
+                for number, studynumber, score in stscore:
+                    if scode in studynumber:
+                        ascore=int(score)
+                        arr.append(ascore)
+                        Avarge=Avarge+ascore
+                        count+=1
+                print("[과목명:%s] [과목코드:%s] [학점:%s] [수강생 수:%d] [평균 점수:%d] [최고점수:%d] [최저점수:%d]" %(studyname,studynumber,time,count,Avarge/count,max(arr),min(arr)))
+
+
+def stats_find2():
     indeptid = input(">학과 번호 입력:")
     dname = get_dept(indeptid)
     print("[학과명 :%s (%s)]" % (dname, indeptid))
-    for number, studynumber, score in stscore:
-        subject = get_subject(studynumber)
-        subject = get_subject(studynumber)
-        study, time, fulltime, FirstName = subject
-        print("[과목명:%s] [학점 : %s] [개설 학과:%s] [ProF:%s]" % (study, time, fulltime, FirstName))
-
+    sumtime=0
+    for id,val in prof.items():
+        name,age=val
+        for scode, val in subject.items():
+            studyname, time, studycode, Nikname = val
+            if id in Nikname:
+                print("[교수아이디:%s][교수명:%s][과목명:%s][학점:%s][수강생수:%s]"%(Nikname,name,studyname,time,len(stscore)))
+                atime=int(time)
+                sumtime=sumtime+atime
+    print("[과목 수:%d] [학점 수:%d] [수강생 합계:%d]"%(len(subject),sumtime,len(stscore)))
 
 ########## Main ################
 while True:  # 작업 선택
